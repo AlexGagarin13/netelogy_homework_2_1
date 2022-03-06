4f19770e75ef833f4c6f73a301ec215fc5768746
Aleksey Anokhin <anoxin-aleksey@bk.ru>
2022-03-06 21:32:13 +0300

    Solution №1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,15 @@
+import java.text.DecimalFormat
+
+const val MINIMUMCOMMISSION = 3500
+const val COMMISSION = 0.0075
+
 fun main() {
+    print("Введите сумму перевода: ")
+    val amount = readLine()?.toDouble() ?: return
+    val amountInpennies = amount * 100
+    val df = DecimalFormat("#.##")
+    if ((amountInpennies * COMMISSION) < MINIMUMCOMMISSION)
+        print("Коммисия составит: ${MINIMUMCOMMISSION / 100} рублей")
+    else print("Коммисия составит: ${df.format(amountInpennies * COMMISSION / 100)} рублей")
 
 }
